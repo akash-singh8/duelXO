7d23878873daca00811856885679cb554bebebe3
Akash Singh <developer.akash8@gmail.com>
2023-10-22 23:02:33 +0600

    update: logo, title & desc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import Head from "next/head";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>DuelXO</title>
+        <meta
+          name="description"
+          content="The ultimate multiplayer Tic Tac Toe experience where strategy meets real-time competition! Challenge your friends or compete against opponents from around the world in a battle for X and O supremacy."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo.png" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
