2db37dc80aa38af023a103d5d66871b4df059c93
Akash Singh <developer.akash8@gmail.com>
2023-10-31 16:23:37 +0600

    home: added header logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import style from "@/styles/Home.module.css";
 export default function Home() {
   return (
     <main className={style.home}>
-      <h1>Hello World</h1>
+      <img src="/logo.svg" alt="duelxo" className="logo" />
     </main>
   );
 }
