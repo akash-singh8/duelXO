16e1331ca68961ba8b8076539e87453cb3dfdb08
Akash Singh <developer.akash8@gmail.com>
2023-10-31 16:31:59 +0600

    home: added tic tac toe image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <main className={style.home}>
       <img src="/logo.svg" alt="duelxo" className="logo" />
+
+      <img src="raw_xo.png" alt="tic tac toe" className={style.rawxo} />
     </main>
   );
 }
