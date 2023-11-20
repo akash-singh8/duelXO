204ccb18e5ae81d1f8f04bfa86f7cb0ad87313e3
Akash Singh <developer.akash8@gmail.com>
2023-11-20 12:26:10 +0530

    server: added WebSocket

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,11 +1,44 @@
 import express from "express";
+import http from "http";
+import { WebSocketServer, WebSocket } from "ws";
 
 const app = express();
+const server = http.createServer(app);
+
+const wss = new WebSocketServer({ server });
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
-app.listen(3053, () => {
-  console.log("app listening on port 3053");
+type wsClient = {
+  [key: number]: WebSocket;
+};
+
+let wsConnections = 0;
+const wsClients: wsClient = {};
+
+wss.on("connection", (ws, req) => {
+  const clientId = wsConnections++;
+  console.log("Client connected", clientId);
+
+  wsClients[clientId] = ws;
+
+  ws.on("message", (message) => {
+    const data = message.toString();
+    console.log("Received Message :", data);
+
+    Object.values(wsClients).forEach((client) => {
+      client.send(data);
+    });
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected", clientId);
+    delete wsClients[clientId];
+  });
+});
+
+server.listen(3053, () => {
+  console.log("Server running at  -   http://localhost:3053");
 });
