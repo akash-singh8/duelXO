65f525442e4e3b29ba8907850f3259e047e66742
Akash Singh <developer.akash8@gmail.com>
2023-11-22 18:07:47 +0530

    handler: added to winner popup

--- packages/ui/components/Winner.tsx
+++ packages/ui/components/Winner.tsx
@@ -16,10 +16,21 @@ export const Winner = ({ winner }: WinnerProps) => {
         <div>
           <button
             style={{ backgroundColor: winner === "X" ? "#58c2ff" : "#ffbb48" }}
+            // can be optimized by resetting the game variables
+            onClick={() => {
+              window.location.reload();
+            }}
           >
             Restart
           </button>
-          <button>close</button>
+          <button
+            // can be optimized by using useRouter hook
+            onClick={() => {
+              window.location.href = "/";
+            }}
+          >
+            close
+          </button>
         </div>
       </div>
     </div>
